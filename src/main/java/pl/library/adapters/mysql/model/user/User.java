607ff7ca8cf4f763374cc948e0026db65552d734
Karetson <robert.kareta@wp.pl>
2021-03-02 18:07:06 +0100

    Added model, service and controller for User. Refactored packages

--- src/main/java/pl/library/adapters/mysql/model/user/User.java
+++ src/main/java/pl/library/adapters/mysql/model/user/User.java
@@ -1,19 +1,18 @@
-package pl.library.domain.model.user;
+package pl.library.adapters.mysql.model.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import pl.library.domain.model.book.Book;
+import lombok.*;
+import pl.library.adapters.mysql.model.book.Book;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -22,14 +21,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
     private String firstName;
+    @NotEmpty
     private String lastName;
     @Email
     private String email;
-    @Pattern( regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
-            message = "The password must be at least 8 characters long. One uppercase letter, one lowercase letter, one number and a special character(@#$%^&+=)")
+    @Pattern( regexp = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^!&+=]).*$",
+            message = "The password must be at least 8 characters long. One uppercase letter, one lowercase letter, one number and a special character(@#$%^!&+=)")
     private String password;
-    private String role;
+    @Builder.Default
+    private UserRole role = UserRole.USER;
     @OneToMany(mappedBy = "user",
             fetch = FetchType.EAGER,
             cascade = CascadeType.PERSIST)
---
+++ src/main/java/pl/library/domain/service/user/UserService.java
@@ -0,0 +1,77 @@
+package pl.library.domain.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.library.adapters.mysql.model.user.User;
+import pl.library.exception.UserExistsException;
+import pl.library.exception.UserNotFoundException;
+import pl.library.repository.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User getById(long id) throws UserNotFoundException {
+        return userRepository.findById(id).orElseThrow(()
+                -> new UserNotFoundException("User with '" + id + "' ID not found!"));
+    }
+
+    public User getByEmail(String email) throws UserNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(()
+                -> new UserNotFoundException("User with '" + email + "' email not found!"));
+    }
+
+    public List<User> getAll() throws UserNotFoundException {
+        if (userRepository.findAll().size() > 0) {
+            return userRepository.findAll();
+        } else
+            throw new UserNotFoundException("There are no users in database");
+    }
+
+    public List<User> getByFirstNameOrLastName(String firstname, String lastName) throws UserNotFoundException {
+        if (userRepository.findByFirstNameOrLastName(firstname, lastName).size() > 0) {
+            return userRepository.findByFirstNameOrLastName(firstname, lastName);
+        } else
+            throw new UserNotFoundException("User with that first or last name not found!");
+    }
+
+    public List<User> getByRole(String role) throws UserNotFoundException {
+        if (userRepository.findByRole(role).size() > 0) {
+            return userRepository.findByRole(role);
+        } else
+            throw new UserNotFoundException("User with " + role + " role not found!");
+    }
+
+    @Transactional
+    public User add(User user) throws UserExistsException {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UserExistsException("User with '" + user.getEmail() + "' email already exists!");
+        } else
+            return userRepository.save(user);
+    }
+
+    @Transactional
+    public User update(long id, User userDetails) throws UserNotFoundException {
+        User user = userRepository.findById(id).orElseThrow(()
+                -> new UserNotFoundException("User with " + id + " ID not found!"));
+
+        user.setFirstName(userDetails.getFirstName());
+        user.setLastName(userDetails.getLastName());
+        user.setEmail(userDetails.getEmail());
+        user.setPassword(userDetails.getPassword());
+
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void delete(long id) throws UserNotFoundException {
+        if (userRepository.findById(id).isPresent()) {
+            userRepository.deleteById(id);
+        } else
+            throw new UserNotFoundException("User with " + id + " ID not found!");
+    }
+}

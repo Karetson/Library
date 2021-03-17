e8eb49897b93cc3c11cfc9566c86f507bc06f56c
Karetson <robert.kareta@wp.pl>
2021-03-17 15:13:46 +0100

    small refresh

--- src/main/java/pl/library/domain/book/exception/BookNotFoundException.java
+++ src/main/java/pl/library/domain/book/exception/BookNotFoundException.java
@@ -1,11 +1,5 @@
 package pl.library.domain.book.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseBody
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(String exception) {
         super(exception);

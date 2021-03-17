27abe1e08123cb4e1d35607cd1e4986c9aebbb43
Karetson <robert.kareta@wp.pl>
2021-03-17 15:13:46 +0100

    small refresh

--- src/main/java/pl/library/adapters/mysql/model/book/Book.java
+++ src/main/java/pl/library/adapters/mysql/model/book/Book.java
@@ -3,7 +3,6 @@ package pl.library.adapters.mysql.model.book;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 @Getter

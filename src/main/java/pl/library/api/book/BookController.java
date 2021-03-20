67eb9a815e05eeb2670de4daf6be861f6643f066
Karetson <robert.kareta@wp.pl>
2021-03-20 11:14:54 +0100

    ref

--- src/main/java/pl/library/api/book/BookController.java
+++ src/main/java/pl/library/api/book/BookController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api/book")
 public class BookController {
     private final BookServiceImpl bookService;
+
     @GetMapping("/search/phrase")
     @ResponseStatus(HttpStatus.OK)
     public List<Book> getAllBooksByPhrase(@RequestParam String phrase)  {

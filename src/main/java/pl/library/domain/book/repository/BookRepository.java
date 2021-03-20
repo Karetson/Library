d6e39ac0b5d92e01db5bd0f45c31ecc1dfb8e601
Karetson <robert.kareta@wp.pl>
2021-03-20 14:04:02 +0100

    Added searching N number of books by random and searching by ID and title

--- src/main/java/pl/library/domain/book/repository/BookRepository.java
+++ src/main/java/pl/library/domain/book/repository/BookRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.query.Param;
 import pl.library.adapters.mysql.model.book.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Query(value = "SELECT * FROM books WHERE title LIKE %:phrase% or author LIKE %:phrase% ORDER BY title ASC", nativeQuery = true)
     List<Book> searchAllByTitleOrAuthorLike(@Param("phrase") String phrase);
+    @Query(value = "SELECT * FROM books ORDER BY RAND() LIMIT :number", nativeQuery = true)
+    List<Book> searchNBooksByRandom(@Param("number") Integer number);
+    Optional<Book> findByIdAndTitle(Long id, String title);
     List<Book> findAllByGenres(String genre);
     Boolean existsByTitleAndAuthor(String title, String author);
 }

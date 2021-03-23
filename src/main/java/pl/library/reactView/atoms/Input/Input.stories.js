c89909e0425849d7e9fcc8d07d3029cc9353449d
Patryk Orłowski <orzeleagle122@gmail.com>
2021-03-23 16:02:33 +0100

    commit structure

--- src/main/java/pl/library/reactView/atoms/Input/Input.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Input from './Input';
-import {storiesOf} from '@storybook/react';
-
-
-storiesOf("Atoms/Input", module)
-.add('Primary',()=><Input>Hello Patrick</Input>)

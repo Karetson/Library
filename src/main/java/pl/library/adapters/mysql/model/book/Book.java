d66322d539783f9081e862054a61630892390795
Patryk Orłowski <orzeleagle122@gmail.com>
2021-04-03 20:43:27 +0200

    fix query and more size varchar

--- src/main/java/pl/library/adapters/mysql/model/book/Book.java
+++ src/main/java/pl/library/adapters/mysql/model/book/Book.java
@@ -1,30 +1,55 @@
 package pl.library.adapters.mysql.model.book;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.library.adapters.mysql.model.borrow.Borrow;
+import pl.library.adapters.mysql.model.genre.Genre;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "books")
+@Entity(name = "books")
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "Title must not be empty")
+    @NotBlank(message = "Title must not be empty")
+    @Size(max = 200)
     private String title;
-    @NotEmpty(message = "Author must not be empty")
+    @NotBlank(message = "Author must not be empty")
+    @Size(max = 200)
     private String author;
-    @NotEmpty(message = "Publisher must not be empty")
+    @NotBlank(message = "Publisher must not be empty")
+    @Size(max = 200)
     private String publisher;
-//    @ManyToOne(cascade = CascadeType.PERSIST)
-//    @JoinColumn(name = "genre_id")
-    private BookGenre genres;
-    private Integer count;
-    private Integer amount = 1;
-
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Genre> genres;
+    @Builder.Default
+    private Integer count = 1;
+    private Integer available;
+    @OneToMany(mappedBy = "book")
+    private List<Borrow> borrows;
+    @Column(length = 2000)
+    @Lob
+    private String description;
+}
 }
